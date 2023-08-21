ae249b3f29f584c4ef2f0ee5f425b74fcffc5790
simplilearn github <gurjotloveparmar@gmail.com>
2023-08-21 19:43:13 +0530

    code pushed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Student from './components/student';
+
+function App() {
+  return (
+    <div className="App">
+      <Student />
+    </div>
+  );
+}
+
+export default App;
